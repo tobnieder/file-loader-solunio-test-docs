5eb7957fc219581b75d0fbdde56ac1a81dfcbe3e
Tobias Niederbrunner <niederbrunner.tobias@gmail.com>
2024-09-06 14:24:09 +0200

    wip: test dark image loader for docs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 
 import { getOptions, interpolateName } from 'loader-utils';
 import { validate } from 'schema-utils';
@@ -33,28 +34,37 @@ export default function loader(content) {
     }
   }
 
-  let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
+  function transf(inputPath) {
+    let resultPath = `__webpack_public_path__ + ${JSON.stringify(inputPath)}`;
 
-  if (options.publicPath) {
-    if (typeof options.publicPath === 'function') {
-      publicPath = options.publicPath(url, this.resourcePath, context);
-    } else {
-      publicPath = `${
-        options.publicPath.endsWith('/')
-          ? options.publicPath
-          : `${options.publicPath}/`
-      }${url}`;
+    if (options.publicPath) {
+      if (typeof options.publicPath === 'function') {
+        resultPath = options.publicPath(url, this.resourcePath, context);
+      } else {
+        resultPath = `${
+          options.publicPath.endsWith('/')
+            ? options.publicPath
+            : `${options.publicPath}/`
+        }${url}`;
+      }
+
+      resultPath = JSON.stringify(resultPath);
     }
 
-    publicPath = JSON.stringify(publicPath);
+    if (options.postTransformPublicPath) {
+      resultPath = options.postTransformPublicPath(resultPath);
+    }
+
+    return resultPath;
   }
 
-  if (options.postTransformPublicPath) {
-    publicPath = options.postTransformPublicPath(publicPath);
-  }
+  const publicPath = transf(outputPath);
+
+  let optionalDark = null;
 
   if (typeof options.emitFile === 'undefined' || options.emitFile) {
     const assetInfo = {};
+    const darkAssetInfo = {};
 
     if (typeof name === 'string') {
       let normalizedName = name;
@@ -66,25 +76,67 @@ export default function loader(content) {
       }
 
       const isImmutable = /\[([^:\]]+:)?(hash|contenthash)(:[^\]]+)?]/gi.test(
-        normalizedName
+        normalizedName,
       );
 
       if (isImmutable === true) {
         assetInfo.immutable = true;
+        darkAssetInfo.immutable = true;
       }
     }
 
     assetInfo.sourceFilename = normalizePath(
-      path.relative(this.rootContext, this.resourcePath)
+      path.relative(this.rootContext, this.resourcePath),
     );
 
+    const extension = path.extname(assetInfo.sourceFilename);
+    const darkImage = path.join(
+      path.dirname(assetInfo.sourceFilename),
+      `${path.basename(assetInfo.sourceFilename, extension)}_dark${extension}`,
+    );
+
+    darkAssetInfo.sourceFilename = darkImage;
+
+    if (fs.existsSync(darkImage)) {
+      console.log('darkImage EXISTS', darkImage, publicPath);
+
+      const extension = path.extname(outputPath);
+      const darkOutputPath = path.join(
+        path.dirname(outputPath),
+        `${path.basename(outputPath, extension)}_dark${extension}`,
+      );
+      const darkString = transf(darkOutputPath);
+
+      optionalDark = `dark: ${darkString}`;
+      console.log('optionalDark', optionalDark);
+      this.emitFile(
+        darkOutputPath,
+        fs.readFileSync(darkImage),
+        null,
+        darkAssetInfo,
+      );
+    }
+
+    console.log('emitted', outputPath);
+
     this.emitFile(outputPath, content, null, assetInfo);
+  } else {
+    console.log('NOT emitted', outputPath, options);
   }
 
   const esModule =
     typeof options.esModule !== 'undefined' ? options.esModule : true;
 
-  return `${esModule ? 'export default' : 'module.exports ='} ${publicPath};`;
+  if (optionalDark === null) {
+    return `${esModule ? 'export default' : 'module.exports ='} ${publicPath};`;
+    /* return `${
+      esModule ? 'export default' : 'module.exports ='
+    } { light:${publicPath}};`; */
+  }
+
+  return `${
+    esModule ? 'export default' : 'module.exports ='
+  } { light:${publicPath}, ${optionalDark}};`;
 }
 
 export const raw = true;
